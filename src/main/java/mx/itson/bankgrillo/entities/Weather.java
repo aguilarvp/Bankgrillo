4d0454899a2bafc7e8c77221be179145ef823dca
alumnog <alumnog@S921-12.itson.edu.mx>
2025-05-06 11:10:47 -0600

    inicio de la base del proyecto

---
+++ src/main/java/mx/itson/bankgrillo/entities/Weather.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.bankgrillo.entities;
+
+/**
+ *
+ * @author alumnog
+ */
+public class Weather {
+    //
+    private String city;
+    private int temperature;
+    private int humidity;
+    
+    //@SerializedName("wind_speed")
+    private int windSpeed;
+    
+}
